bc723fe89d8eb24eb2e620524596a3b571c18082
Bruno Basto <bruno.basto@liferay.com>
2020-05-22 22:54:23 -0300

    Adds API for the remaining elementary operations

---
+++ src/ts/api/subtract.ts
@@ -0,0 +1,10 @@
+import { Operation, Value } from '../types';
+import binary from '../binary';
+
+export default (a: Value, b: Value): Value => {
+    return binary(
+        a,
+        b,
+        Operation.SUBTRACT
+    );
+}
