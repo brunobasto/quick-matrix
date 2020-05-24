7d6b50ea8103a26df0d05ab38e3bc7edc14e6113
Bruno Basto <bruno.basto@liferay.com>
2020-05-24 11:32:25 -0300

    Exports missing functions

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,9 @@
 export {default as add} from './api/add';
+export {default as concat} from './api/concat';
 export {default as divide} from './api/divide';
 export {default as exp} from './api/exp';
 export {default as multiply} from './api/multiply';
+export {default as ones} from './api/ones';
 export {default as subtract} from './api/subtract';
 export {default as transpose} from './api/transpose';
+export {default as zeros} from './api/zeros';
