d1594e33892b387df5f63370b257b59dd9261db9
Bruno Basto <bruno.basto@liferay.com>
2020-05-24 11:26:39 -0300

    Typo

--- src/ts/typings.d.ts
+++ src/ts/typings.d.ts
@@ -6,7 +6,7 @@ export type divide = (a: Value, b: Value) => Value;
 export type exp = (a: Value) => Value;
 export type fill = (shape: Shape, value: Scalar) => VectorOrMatrix;
 export type from = (value: any) => Value;
-export type multitply = (a: Value, b: Value) => Value;
+export type multiply = (a: Value, b: Value) => Value;
 export type ones = (shape: Shape) => VectorOrMatrix;
 export type product = (a: Matrix, b: Matrix) => Matrix;
 export type shape = (a: Value) => Shape;
